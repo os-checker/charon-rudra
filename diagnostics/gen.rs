034b5b3dcdca6d158c531c6d7c8af2fdbf24b78a
zjp <jiping_zhou@foxmail.com>
2025-03-05 00:21:10 +0800

    diagnostics: get all test rs files

--- diagnostics/gen.rs
+++ diagnostics/gen.rs
@@ -1,2 +1,23 @@
+use expect_test::expect_file;
+
 #[test]
-fn gen() {}
+fn gen() {
+    let mut tests = Vec::with_capacity(24);
+
+    // Find all test rs files
+    for res in walkdir::WalkDir::new("tests") {
+        let entry = res.unwrap();
+        if entry.file_type().is_file() {
+            let path = entry.path();
+            if path.extension() == Some("rs".as_ref()) {
+                // Uniform path to linux style
+                let s = entry.into_path().to_str().unwrap().replace("\\", "/");
+                tests.push(s);
+            }
+        }
+    }
+
+    expect_file!["tests.txt"].assert_debug_eq(&tests);
+
+    dbg!(tests);
+}
