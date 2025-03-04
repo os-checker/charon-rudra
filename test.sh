321f4722686797e9705a5dd592422ffcf2c664cd
zjp <jiping_zhou@foxmail.com>
2025-03-04 21:01:18 +0800

    ci: add & run tests

---
+++ test.sh
@@ -0,0 +1,13 @@
+cd charon/charon
+# Install charon / charon-driver / generate-ml
+cargo install --path . --locked
+
+cd ../..
+# Install cargo-charon-rudra
+cargo install --path . --locked
+
+# Generate insertion_sort.ullbc
+charon --ullbc --no-merge-goto-chains --no-cargo --input tests/panic_safety/insertion_sort.rs
+
+# Analyze with rudra
+cargo-charon-rudra --file insertion_sort.ullbc
