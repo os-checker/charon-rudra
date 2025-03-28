290286179fc5b207681ac9b62615593843c21b77
zjp <jiping_zhou@foxmail.com>
2025-03-28 21:38:15 +0800

    basic SendChecker with trait_didd & send_impls fields

---
+++ src/rudra/analysis/send_sync_variance/analyze_send.rs
@@ -0,0 +1,23 @@
+use super::utils::{Krate, TraitDid};
+use crate::rudra::context::RudraCtxt;
+use charon_lib::ast::TraitImpl;
+
+pub struct SendChecker<'tcx> {
+    rcx: RudraCtxt<'tcx>,
+    trait_dids: TraitDid,
+    send_impls: Vec<&'tcx TraitImpl>,
+}
+
+impl<'tcx> SendChecker<'tcx> {
+    pub fn new(rcx: RudraCtxt<'tcx>) -> Self {
+        let krate = Krate::new(&rcx.crate_data);
+        let trait_dids = krate.send_sync_copy();
+        Self {
+            rcx,
+            send_impls: krate.trait_impls(trait_dids.send),
+            trait_dids,
+        }
+    }
+
+    pub fn analyze(self) {}
+}
---
+++ src/rudra/analysis/send_sync_variance/utils.rs
@@ -0,0 +1,54 @@
+use charon_lib::ast::*;
+
+#[derive(Clone, Copy)]
+pub struct Krate<'a> {
+    krate: &'a TranslatedCrate,
+}
+
+impl<'a> Krate<'a> {
+    pub fn new(data: &TranslatedCrate) -> Krate {
+        Krate { krate: data }
+    }
+
+    /// Get def id of a trait.
+    pub fn trait_def_id(self, name: &[&str]) -> Option<TraitDeclId> {
+        self.krate
+            .trait_decls
+            .iter()
+            .find(|t| t.item_meta.name.equals_ref_name(name))
+            .map(|t| t.def_id)
+    }
+
+    /// Decl ids of Send, Sync, and Copy.
+    pub fn send_sync_copy(self) -> TraitDid {
+        TraitDid {
+            send: self.trait_def_id(SEND),
+            sync: self.trait_def_id(SYNC),
+            copy: self.trait_def_id(COPY),
+        }
+    }
+
+    /// All local impls of a trait.
+    pub fn trait_impls(self, trait_did: Option<TraitDeclId>) -> Vec<&'a TraitImpl> {
+        let Some(trait_did) = trait_did else {
+            return vec![];
+        };
+        self.krate
+            .trait_impls
+            .iter()
+            .filter(|i| i.impl_trait.trait_id == trait_did)
+            .collect()
+    }
+}
+
+const SEND: &[&str] = &["core", "marker", "Send"];
+const SYNC: &[&str] = &["core", "marker", "Sync"];
+const COPY: &[&str] = &["core", "marker", "Copy"];
+
+/// Trait decl id in a crate. None if the trait doesn't appear in the crate.
+#[derive(Debug, Clone)]
+pub struct TraitDid {
+    pub send: Option<TraitDeclId>,
+    pub sync: Option<TraitDeclId>,
+    pub copy: Option<TraitDeclId>,
+}
